4e4a5d1ae6c02b44ac23f2d5da0ad03fe70afb29
Kofi Boateng <kdboat2@gmail.com>
2022-03-19 15:46:40 -0500

    Finished with authentication. Will now move to working on customers account on the front and backend.

--- src/store/store.ts
+++ src/store/store.ts
@@ -1,9 +1,11 @@
 import { configureStore } from "@reduxjs/toolkit";
 import { authSlice } from "./authentication/auth-slice";
+import { customerSlice } from "./customer/customer";
 
 const store = configureStore({
   reducer: {
     auth: authSlice.reducer,
+    cust: customerSlice.reducer,
   },
 });
 
