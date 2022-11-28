de601d9708da1d9cb6c5f89ad63f4619a3578b2b
Kofi Boateng <kdboat2@gmail.com>
2022-11-27 19:22:07 -0600

    Edited verifyAccount component.

--- src/pages/VerifyAccount.tsx
+++ src/pages/VerifyAccount.tsx
@@ -18,43 +18,40 @@ const VerifyAccount: FC<{
   LoadingSpinner: FC<{}>;
 }> = ({ axios, API_VERSION, LoadingSpinner, isMobile }) => {
   const navigate: NavigateFunction = useNavigate();
-  const [loading, setLoading] = useState<boolean>(true);
-  const [verified, setVerified] = useState<boolean | null>(null);
-  const searchParams = useSearchParams()[0];
+  const params = useSearchParams()[0];
+  const tokenParam = params.get("token");
+  const accountVerifiedParam = params.get("verified");
   useEffect(() => {
     const fetchVerification: (token: string | null) => void = async (token) => {
       await axios
         .get(`${API_VERSION}/authentication/confirm-account`, {
           params: { token: token },
         })
-        .then((repsonse) => {
-          setLoading(false);
-          setVerified(repsonse.data);
+        .then(() => {
+          navigate(`?verified=successful`);
           setTimeout(() => {
             navigate(AppRoute.LOGIN, { replace: true });
           }, 5000);
         })
         .catch(() => {
-          setLoading(false);
-          setVerified(false);
+          navigate(`?verified=unsuccessful`);
           setTimeout(() => {
             navigate(AppRoute.LOGIN, { replace: true });
           }, 5000);
         });
     };
-    fetchVerification(searchParams.get("token"));
+    fetchVerification(tokenParam);
   });
   return (
     <>
-      {loading ? (
-        <LoadingSpinner />
-      ) : verified ? (
+      {!accountVerifiedParam && <LoadingSpinner />}
+      {accountVerifiedParam && accountVerifiedParam == "successful" && (
         <True isMobile={isMobile} Grid={Grid} Box={Box} PASSED={Check} />
-      ) : (
+      )}
+      {accountVerifiedParam && accountVerifiedParam == "unsuccessful" && (
         <False isMobile={isMobile} Grid={Grid} Box={Box} FAILED={Cancel} />
       )}
     </>
   );
 };
-
 export default VerifyAccount;
